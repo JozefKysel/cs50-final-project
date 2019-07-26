2e4d0218dd2a148365acf838e91f0a54a3e0bf67
JozefKysel <jojo.kysel00@gmail.com>
2019-07-26 19:26:38 +0200

    Added readme.md

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'books'
+app.config['MYSQL_DB'] = ''
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 app.config['JWT_SECRET_KEY'] = ''
 
