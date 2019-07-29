223fb24ec56785ab78d9ba8bd1c1fbce3118c48b
JozefKysel <jojo.kysel00@gmail.com>
2019-07-29 08:08:09 +0200

    Fixes

--- client/src/components/library.js
+++ client/src/components/library.js
@@ -3,9 +3,10 @@ import Book from './book';
 import './library.css';
 
 function Library({ bookList, fetchMyBooks }) {
+  let index = 0;
   const unique_key = (book) => book.author + book.title + book.rating;
   return bookList.length > 0 ? bookList.map(book =>
-    <div className="library" key={unique_key(book)}>
+    <div className="library" key={++index}>
       <Book fetchMyBooks={fetchMyBooks} bookList={bookList} book={book}/>
     </div>
   ) : <div className="search">Search for books....</div>
--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ from json import dumps
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = ''
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = ''
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
